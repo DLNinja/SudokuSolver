72827e22b9b9f66a44029fec4578d75d2749ebda
MLninja <tudi.gadalean@gmail.com>
2020-06-11 19:12:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+# board
+sudoku = np.array([[5, 3, 0, 0, 7, 0, 0, 0, 0],
+                   [6, 0, 0, 1, 9, 5, 0, 0, 0],
+                   [0, 9, 8, 0, 0, 0, 0, 6, 0],
+                   [8, 0, 0, 0, 6, 0, 0, 0, 3],
+                   [4, 0, 0, 8, 0, 3, 0, 0, 1],
+                   [7, 0, 0, 0, 2, 0, 0, 0, 6],
+                   [0, 6, 0, 0, 0, 0, 2, 8, 0],
+                   [0, 0, 0, 4, 1, 9, 0, 0, 5],
+                   [0, 0, 0, 0, 8, 0, 0, 7, 9]])
+
+def board(sudo):
+    # print board
+    print("-------------------------")
+    for i in range(9):
+        if i == 3 or i == 6:
+            print("-------------------------")
+        print("|", end=" ")
+        for j in range(9):
+            if j == 3 or j == 6:
+                print("|", end=" ")
+            print(sudo[i][j], end=" ")
+        print("|")
+    print("-------------------------")
+
+def verify(sudo, i, j, nr):
+    # verify if the value can be placed on position (i, j)
+
+    a = i // 3
+    b = j // 3
+
+    # verifying line and column for the given value
+    for k in range(9):
+        if sudo[i][k] == nr and k != j:
+            return False
+    for k in range(9):
+        if sudo[k][j] == nr and k != i:
+            return False
+
+    # verifying 'box' for given value
+    for k in range(a * 3, a * 3 + 3):
+        for f in range(b * 3, b * 3 + 3):
+            if sudo[k][f] == nr and k != i and f != j:
+                return False
+
+    return True
+
+
+def zero(sudo):
+    # searching for a zero value
+    for i in range(9):
+        for j in range(9):
+            if sudo[i][j] == 0:
+                return i, j
+    return False
+
+
+def solution(sudo):
+    # searches for zero on the board
+    poz = zero(sudo)
+
+    if not poz:
+        # if there are no zeros we print the board
+        board(sudo)
+        return True
+    else:
+        # else, we continue with the zero value
+        i, j = poz
+
+    for nr in range(1, 10):
+        if verify(sudo, i, j, nr):
+            # finding a value to match the current spot
+            sudo[i][j] = nr
+
+            if solution(sudo):
+                return True
+
+            sudo[i][j] = 0
+
+    return False
+
+
+solution(sudoku)
